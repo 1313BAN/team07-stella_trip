3b5cb23ea052556f95eccd4b431220bb372d2c70
luna156 <v010309@naver.com>
2025-05-02 15:52:23 +0900

    [Chore] initial commit

---
+++ back_end/stella_trip/src/main/java/com/ssafy/stella_trip/StellaTripApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.stella_trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StellaTripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StellaTripApplication.class, args);
+    }
+
+}
---
+++ back_end/stella_trip/src/test/java/com/ssafy/stella_trip/StellaTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.stella_trip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StellaTripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
