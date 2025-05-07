47a030c25af383f52697e842bf71ab7b484029ee
SeungYeon Han <hanstom000608@gmail.com>
2025-05-07 16:44:44 +0900

    feat: public 제한자, 인증 검사

--- back_end/src/main/java/com/ssafy/stella_trip/user/controller/UserProfileController.java
+++ back_end/src/main/java/com/ssafy/stella_trip/user/controller/UserProfileController.java
@@ -8,36 +8,39 @@ import com.ssafy.stella_trip.user.dto.response.UserProfileResponseDTO;
 import com.ssafy.stella_trip.user.service.UserProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
+@PreAuthorize("isAuthenticated()")
 public class UserProfileController {
 
     private final UserProfileService userProfileService;
 
     //자신의 프로필 조회
     @GetMapping
-    CommonResponse<MyProfileResponseDTO> getMyProfile() {
+    public CommonResponse<MyProfileResponseDTO> getMyProfile() {
         return new CommonResponse<>(userProfileService.getMyProfile(), HttpStatus.OK);
     }
 
     //다른 유저의 프로필 조회
     @GetMapping("/{userId}")
-    CommonResponse<UserProfileResponseDTO> getUserProfile(@PathVariable int userId) {
+    @PreAuthorize("permitAll()")
+    public CommonResponse<UserProfileResponseDTO> getUserProfile(@PathVariable int userId) {
         return new CommonResponse<>(userProfileService.getUserProfile(userId), HttpStatus.OK);
     }
 
     //자신의 프로필 수정
     @PutMapping("/profile")
-    CommonResponse<MyProfileResponseDTO> updateMyProfile(@RequestBody MyProfileUpdateRequestDTO myProfileUpdateRequestDTO) {
+    public CommonResponse<MyProfileResponseDTO> updateMyProfile(@RequestBody MyProfileUpdateRequestDTO myProfileUpdateRequestDTO) {
         return new CommonResponse<>(userProfileService.updateMyProfile(myProfileUpdateRequestDTO), HttpStatus.OK);
     }
 
     //자신의 비밀번호 수정
     @PutMapping("/password")
-    CommonResponse<Void> updatePassword(@RequestBody PasswordUpdateRequestDTO passwordUpdateRequestDTO) {
+    public CommonResponse<Void> updatePassword(@RequestBody PasswordUpdateRequestDTO passwordUpdateRequestDTO) {
         userProfileService.updatePassword(passwordUpdateRequestDTO.getPassword());
         return new CommonResponse<>(null, HttpStatus.NO_CONTENT);
     }
