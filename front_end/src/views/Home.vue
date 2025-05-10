f9fd1522bb306c22169dab79dc9d0a3a3c34f641
SeungYeon Han <hanstom000608@gmail.com>
2025-05-10 15:29:23 +0900

    fix: 홈페이지 수정

--- front_end/src/views/Home.vue
+++ front_end/src/views/Home.vue
@@ -1,18 +1,8 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<script setup lang="ts">
-import { useRouter } from 'vue-router';
+<script setup lang="ts"></script>
 
-const router = useRouter();
-
-function goHome(): void {
-  router.push('/');
-}
-</script>
 <template>
   <div>
-    <h2>404 페이지</h2>
-    <div>
-      <button @click="goHome">홈으로</button>
-    </div>
+    <h2>홈페이지</h2>
   </div>
 </template>
