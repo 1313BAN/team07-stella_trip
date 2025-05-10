bd1d29b70803fffc4979184cc992f6780286ad00
SeungYeon Han <hanstom000608@gmail.com>
2025-05-10 15:26:25 +0900

    feat: router 기본 구조

---
+++ front_end/src/router/index.ts
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
+import { setupInterceptors } from './intorceptors';
+import { ROUTES } from './routes';
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: ROUTES.HOME.path,
+    name: ROUTES.HOME.name,
+    component: () => import('@/views/Home.vue'),
+    meta: {
+      title: ROUTES.HOME.title,
+    },
+  },
+  {
+    path: ROUTES.NOT_FOUND.path,
+    name: ROUTES.NOT_FOUND.name,
+    component: () => import('@/views/NotFound.vue'),
+    meta: {
+      title: ROUTES.NOT_FOUND.title,
+    },
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+setupInterceptors(router);
+
+export default router;
---
+++ front_end/src/views/Home.vue
@@ -0,0 +1,18 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+function goHome(): void {
+  router.push('/');
+}
+</script>
+<template>
+  <div>
+    <h2>404 페이지</h2>
+    <div>
+      <button @click="goHome">홈으로</button>
+    </div>
+  </div>
+</template>
