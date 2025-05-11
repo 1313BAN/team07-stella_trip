444f90272fdd23b8ebbdddbf8bb12107caf3f8eb
SeungYeon Han <hanstom000608@gmail.com>
2025-05-11 14:07:19 +0900

    feat: attraction, review DTO 추가

--- back_end/src/main/java/com/ssafy/stella_trip/attraction/dto/response/AttractionResponseDTO.java
+++ back_end/src/main/java/com/ssafy/stella_trip/attraction/dto/response/AttractionResponseDTO.java
@@ -1,5 +1,28 @@
 package com.ssafy.stella_trip.attraction.dto.response;
 
-public class AttractionResponseDTO {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AttractionResponseDTO {
+    @JsonProperty("attraction_id")
+    private int attractionId;
+    private String name;
+    private String image;
+    private String address;
+    @JsonProperty("content_type")
+    private String contentType;
+    private int like;
+    private double rating;
+    private double latitude;
+    private double longitude;
+    private List<ReviewResponseDTO> review;
 }
