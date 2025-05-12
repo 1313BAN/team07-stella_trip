0ad459d2d91ec3de921ba04fbbb9c9cc1f63b3e2
luna156 <v010309@naver.com>
2025-05-12 16:48:57 +0900

    fix: front 반환 네이밍 통일

--- back_end/src/main/java/com/ssafy/stella_trip/plan/dto/response/LockStatusResponseDTO.java
+++ back_end/src/main/java/com/ssafy/stella_trip/plan/dto/response/LockStatusResponseDTO.java
@@ -1,5 +1,6 @@
 package com.ssafy.stella_trip.plan.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LockStatusResponseDTO {
+    @JsonProperty("user_id")
     private int userId;
+    @JsonProperty("plan_id")
     private int planId;
+    @JsonProperty("lock_status")
     private boolean lockStatus;
 }
