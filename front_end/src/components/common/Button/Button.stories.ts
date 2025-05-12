1fd66c9fe02918d20cb857cf46e84b7e6b601d99
SeungYeon Han <hanstom000608@gmail.com>
2025-05-12 20:20:43 +0900

    feat: storybook button tutorial

---
+++ front_end/src/components/common/Button/Button.stories.ts
@@ -0,0 +1,112 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+import { fn } from '@storybook/test';
+import Button, { type ButtonInterface } from './Button.vue';
+
+interface ButtonProps extends ButtonInterface {
+  variant?: 'default' | 'destructive' | 'outline' | 'secondary' | 'ghost' | 'link';
+  size?: 'default' | 'sm' | 'lg' | 'icon';
+  disabled?: boolean;
+  class?: string;
+  onClick: () => void;
+}
+
+const meta: Meta<ButtonProps> = {
+  title: 'Components/Button',
+  component: Button,
+  tags: ['autodocs'],
+  args: {
+    onClick: fn(),
+  },
+  argTypes: {
+    variant: {
+      control: 'select',
+      options: ['default', 'destructive', 'outline', 'secondary', 'ghost', 'link'],
+    },
+    size: {
+      control: 'select',
+      options: ['default', 'sm', 'lg', 'icon'],
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    variant: 'default',
+    size: 'default',
+  },
+  render: args => ({
+    components: { Button },
+    setup() {
+      return { args };
+    },
+    template: '<Button v-bind="args">Default Button</Button>',
+  }),
+};
+
+export const Destructive: Story = {
+  args: {
+    variant: 'destructive',
+  },
+  render: args => ({
+    components: { Button },
+    setup() {
+      return { args };
+    },
+    template: '<Button v-bind="args">Delete</Button>',
+  }),
+};
+
+export const Outline: Story = {
+  args: {
+    variant: 'outline',
+  },
+  render: args => ({
+    components: { Button },
+    setup() {
+      return { args };
+    },
+    template: '<Button v-bind="args">Outline</Button>',
+  }),
+};
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+  },
+  render: args => ({
+    components: { Button },
+    setup() {
+      return { args };
+    },
+    template: '<Button v-bind="args">Secondary</Button>',
+  }),
+};
+
+export const Ghost: Story = {
+  args: {
+    variant: 'ghost',
+  },
+  render: args => ({
+    components: { Button },
+    setup() {
+      return { args };
+    },
+    template: '<Button v-bind="args">Ghost</Button>',
+  }),
+};
+
+export const Link: Story = {
+  args: {
+    variant: 'link',
+  },
+  render: args => ({
+    components: { Button },
+    setup() {
+      return { args };
+    },
+    template: '<Button v-bind="args">Link</Button>',
+  }),
+};
---
+++ front_end/src/components/common/Button/Button.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface ButtonInterface {
+  //TODO: 버튼 커스텀 속성
+}
+
+defineEmits<{
+  click: [event: MouseEvent];
+}>();
+</script>
+
+<template>
+  <Button v-bind="$attrs" @click="$emit('click', $event)">
+    <slot />
+  </Button>
+</template>
