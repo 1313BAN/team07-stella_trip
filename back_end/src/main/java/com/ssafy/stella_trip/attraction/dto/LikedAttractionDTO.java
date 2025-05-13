f892316f41012cbe232d7fc843c05024053a6210
SeungYeon Han <hanstom000608@gmail.com>
2025-05-13 19:42:34 +0900

    fix: 누락된 lombok 어노테이션 추가

--- back_end/src/main/java/com/ssafy/stella_trip/attraction/dto/LikedAttractionDTO.java
+++ back_end/src/main/java/com/ssafy/stella_trip/attraction/dto/LikedAttractionDTO.java
@@ -1,5 +1,14 @@
 package com.ssafy.stella_trip.attraction.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LikedAttractionDTO {
     private int userId;
     private int attractionId;
