e54e079d0e9235291d2818985734410e479a63c5
SeungYeon Han <hanstom000608@gmail.com>
2025-05-14 21:46:14 +0900

    chore: vue dev tools 의존성 추가

--- front_end/vite.config.ts
+++ front_end/vite.config.ts
@@ -1,11 +1,13 @@
 import path from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import vueDevTools from 'vite-plugin-vue-devtools';
 import { defineConfig } from 'vite';
 
 //https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), vueDevTools(), createHtmlPlugin()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
