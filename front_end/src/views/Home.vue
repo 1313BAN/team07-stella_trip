9f105891edf6e0982085e5ed56268deb918698be
SeungYeon Han <hanstom000608@gmail.com>
2025-05-16 10:10:05 +0900

    feat: 임시 intro 섹션

--- front_end/src/views/Home.vue
+++ front_end/src/views/Home.vue
@@ -2,10 +2,12 @@
   <HeroBackground />
   <main>
     <HeroSection />
+    <IntroductionSection />
   </main>
 </template>
 
 <script setup lang="ts">
 import HeroBackground from '@/components/background/HeroBackground.vue';
 import HeroSection from '@/components/views/home/HeroSection.vue';
+import IntroductionSection from '@/components/views/home/IntroductionSection.vue';
 </script>
