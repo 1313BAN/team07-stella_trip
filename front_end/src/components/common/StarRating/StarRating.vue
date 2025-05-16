65f92a54562e052f740f746fb498443d9446955f
SeungYeon Han <hanstom000608@gmail.com>
2025-05-16 10:52:48 +0900

    feat: rating 예외 처리

--- front_end/src/components/common/StarRating/StarRating.vue
+++ front_end/src/components/common/StarRating/StarRating.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { Star, StarHalf } from 'lucide-vue-next';
 import { getStarData } from '@/utils/util';
+import { computed } from 'vue';
 
 interface Props {
   rating: number;
@@ -9,10 +10,15 @@ interface Props {
 const props = defineProps<Props>();
 
 const stars = getStarData(props.rating);
+const formattedRating = computed(() => props.rating.toFixed(1));
+const ariaLabel = computed(() => `평점 ${formattedRating.value}점 (5점 만점)`);
 </script>
 
 <template>
-  <div class="flex w-fit items-center gap-2 rounded-full bg-black/50 px-3 py-2 backdrop-blur-sm">
+  <div
+    class="flex w-fit items-center gap-2 rounded-full bg-black/50 px-3 py-2 backdrop-blur-sm"
+    :ariaLable="ariaLabel"
+  >
     <div class="flex items-center">
       <Star
         v-for="i in stars.full"
@@ -23,7 +29,7 @@ const stars = getStarData(props.rating);
       <Star v-for="i in stars.empty" :key="`empty-${i}`" class="h-4 w-4 text-gray-400" />
     </div>
     <span class="text-sm font-semibold text-white">
-      {{ rating.toFixed(1) }}
+      {{ formattedRating }}
     </span>
   </div>
 </template>
--- front_end/src/utils/util.ts
+++ front_end/src/utils/util.ts
@@ -7,13 +7,14 @@ export interface StarsData {
 /**
  * 별점 숫자를 기반으로 꽉 찬 별, 반 별, 빈 별 개수를 계산
  *
- * @param rating - 0부터 5까지의 실수형 별점 값
+ * @param rating - 0부터 5까지의 실수형 별점 값 (벗어나는 경우 0)
  * @returns 별점에 해당하는 별의 구성 정보
  * - full: 꽉찬 별 개수
  * - half: 반쪽 별 개수
  * - empty: 빈 별 개수
  */
 export const getStarData = (rating: number): StarsData => {
+  if (rating < 0 || rating > 5) rating = 0;
   const fullStars = Math.floor(rating);
   const hasHalfStar = rating % 1 >= 0.5;
   const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
