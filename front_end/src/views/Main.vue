677e8a0b24349d74441fae808f58b88efd750aed
SeungYeon Han <hanstom000608@gmail.com>
2025-05-17 18:36:33 +0900

    feat: 메인 페이지 Hero Section

--- front_end/src/views/Main.vue
+++ front_end/src/views/Main.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import HeroBackground from '@/components/background/HeroBackground.vue';
 import TagOverviewSection from '@/components/views/main/TagOverviewSection.vue';
+import HeroSection from '@/components/views/main/HeroSection.vue';
 </script>
 
 <template>
   <HeroBackground />
-  <div>
+  <div class="min-h-screen">
+    <HeroSection />
     <TagOverviewSection />
   </div>
 </template>
