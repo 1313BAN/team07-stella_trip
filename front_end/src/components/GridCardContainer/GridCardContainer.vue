42a5691e60b927a048a86a4577b588d621426c1c
SeungYeon Han <hanstom000608@gmail.com>
2025-05-17 17:04:48 +0900

    feat: grid card container 요소 크기 통일

--- front_end/src/components/GridCardContainer/GridCardContainer.vue
+++ front_end/src/components/GridCardContainer/GridCardContainer.vue
@@ -7,31 +7,19 @@ defineProps<Props>();
 </script>
 
 <template>
-  <div class="grid-card-container">
-    <h2 class="mb-4 text-2xl font-bold text-white">{{ title }}</h2>
+  <div class="w-full px-4">
+    <h2 class="text-2xl font-bold text-white">{{ title }}</h2>
 
     <!-- Flex 컨테이너 -->
-    <div class="flex w-full flex-wrap gap-4">
+    <div class="flex w-full flex-wrap gap-4 pt-4">
       <slot></slot>
+      <div class="invisible max-w-72 min-w-56 flex-1 p-4"></div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.grid-card-container {
-  width: 100%;
-  padding: 1rem 0;
-}
-
-/* 슬롯에 들어가는 직계 자식 요소들 */
 :deep(.flex > *) {
   flex: 1 1 0 !important;
-  /* 
-    - flex-grow: 1 → 공간이 남으면 균등하게 확장 (min-width 보다 크게)
-    - flex-shrink: 1 → 공간이 부족하면 균등하게 축소 (min-width까지만)
-    - flex-basis: 0 → 균등한 너비 배분의 시작점
-  */
-
-  /* 기존 min-width와 max-width는 유지됨 */
 }
 </style>
