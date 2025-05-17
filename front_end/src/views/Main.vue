c25de40b4433929e2d877147a6e7469c6eee0a0d
SeungYeon Han <hanstom000608@gmail.com>
2025-05-17 16:02:41 +0900

    feat: add main page

---
+++ front_end/src/views/Main.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HeroBackground from '@/components/background/HeroBackground.vue';
+import TagOverviewSection from '@/components/views/main/TagOverviewSection.vue';
+</script>
+
+<template>
+  <HeroBackground />
+  <div>
+    <TagOverviewSection />
+  </div>
+</template>
