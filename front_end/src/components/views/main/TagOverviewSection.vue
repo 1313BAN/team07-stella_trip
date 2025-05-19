d34f994f3b641b5f863c5bdb8b3fc1574c92247d
SeungYeon Han <hanstom000608@gmail.com>
2025-05-20 00:55:35 +0900

    feat: tag overview section에 aysnc container적용

--- front_end/src/components/views/main/TagOverviewSection.vue
+++ front_end/src/components/views/main/TagOverviewSection.vue
@@ -1,95 +1,51 @@
 <template>
   <section class="relative min-h-screen">
     <div class="container mx-auto">
-      <GridCardListContainer title="인기 여행 태그">
-        <TagCard
-          v-for="tag in popularTags"
-          :key="tag.tagId"
-          :label="tag.name"
-          :labelCount="tag.count"
-        ></TagCard>
-      </GridCardListContainer>
-      <RowCardListContainer
-        title="힐링"
-        moreLink="/categories/healing"
-        @moreClick="() => handleMoreClick('힐링')"
-      >
-        <PlanCard
-          v-for="plan in healingPlans"
-          :key="plan.planId"
-          :plan="plan"
-          @cardClick="handlePlanCardClick"
-          @likeClick="handlePlanLikeClick"
-          @tagClick="handlePlanTagClick"
-        />
-      </RowCardListContainer>
+      <!-- 인기 태그 섹션 -->
+      <AsyncContainer :loadingComponent="PopularTagsSkeleton" :errorComponent="PopularTagsError">
+        <PopularTagsSection />
+      </AsyncContainer>
 
-      <RowCardListContainer
-        title="인기 관광지"
-        moreLink="/attractions/popular"
-        @moreClick="() => handleMoreClick('인기 관광지')"
+      <!-- 인기 명소 섹션 -->
+      <AsyncContainer
+        :loadingComponent="AttractionsSkeleton"
+        :errorComponent="AttractionsError"
+        title="인기 명소"
       >
-        <AttractionCard
-          v-for="attraction in popularAttractions"
-          :key="attraction.attractionId"
-          :attraction="attraction"
-          :showRating="true"
+        <AttractionsSection
+          title="인기 명소"
           @cardClick="handleAttractionCardClick"
           @likeClick="handleAttractionLikeClick"
           @tagClick="handleAttractionTagClick"
+          @moreClick="() => handleMoreClick(12)"
         />
-      </RowCardListContainer>
+      </AsyncContainer>
+
+      <!-- 인기 문화 시설 섹션 -->
+      <AsyncContainer
+        :loadingComponent="AttractionsSkeleton"
+        :errorComponent="AttractionsError"
+        title="인기 문화 시설"
+      >
+        <AttractionsSection
+          title="인기 문화 시설"
+          :apiParams="{ contentTypeId: 14 }"
+          @cardClick="handleAttractionCardClick"
+          @likeClick="handleAttractionLikeClick"
+          @tagClick="handleAttractionTagClick"
+          @moreClick="() => handleMoreClick(12)"
+        />
+      </AsyncContainer>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import RowCardListContainer from '@/components/common/RowCardListContainer/RowCardListContainer.vue';
-import PlanCard from '@/components/PlanCard/PlanCard.vue';
-import AttractionCard from '@/components/AttractionCard/AttractionCard.vue';
-import GridCardListContainer from '@/components/GridCardListContainer/GridCardListContainer.vue';
-import TagCard from '@/components/TagCard/TagCard.vue';
-import { getPlans, getPopularTags, type Plan, type Tag } from '@/services/api/domains/plan';
-import { getAttractions, type Attraction } from '@/services/api/domains/attraction';
+import AsyncContainer from '@/components/common/AsyncContainer/AsyncContainer.vue';
 
-const healingPlans = ref<Plan[]>([]);
-const popularAttractions = ref<Attraction[]>([]);
-const popularTags = ref<Tag[]>([]);
-
-//TODO: 이후 Suspence 와 ErrorBoundary로 관리 예정
-const setupData = async () => {
-  const [plansResponse, attractionsResponse, tagsResponse] = await Promise.all([
-    getPlans({
-      page: 1,
-      size: 10,
-      sort: 'RECENT',
-    }),
-    getAttractions({
-      page: 1,
-      size: 10,
-    }),
-    getPopularTags(),
-  ]);
-
-  healingPlans.value = plansResponse.content;
-  popularAttractions.value = attractionsResponse.content;
-  popularTags.value = tagsResponse;
-};
-
-setupData();
-
-const handlePlanCardClick = (plan: Plan) => {
-  console.log('여행 계획 카드 클릭:', plan.title);
-};
-
-const handlePlanLikeClick = (plan: Plan) => {
-  console.log('여행 계획 좋아요 클릭:', plan.title);
-};
-
-const handlePlanTagClick = (tag: Tag) => {
-  console.log('여행 계획 태그 클릭:', tag);
-};
+import { type Attraction } from '@/services/api/domains/attraction';
+import { PopularTagsError, PopularTagsSection, PopularTagsSkeleton } from './PopularTagsSection';
+import { AttractionsError, AttractionsSection, AttractionsSkeleton } from './AttractionsSection';
 
 const handleAttractionCardClick = (attraction: Attraction) => {
   console.log('관광지 카드 클릭:', attraction.name);
@@ -99,11 +55,11 @@ const handleAttractionLikeClick = (attraction: Attraction) => {
   console.log('관광지 좋아요 클릭:', attraction.name);
 };
 
-const handleAttractionTagClick = (contentType: string) => {
+const handleAttractionTagClick = (contentType: number) => {
   console.log('관광지 태그(컨텐츠 타입) 클릭:', contentType);
 };
 
-const handleMoreClick = (category: string) => {
-  console.log('더보기 클릭:', category);
+const handleMoreClick = (contentType: number) => {
+  console.log('더보기 클릭:', contentType);
 };
 </script>
