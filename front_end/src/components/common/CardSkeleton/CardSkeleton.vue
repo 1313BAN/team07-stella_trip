a44b195416a789779cc939b8e69f41600cf3c452
SeungYeon Han <hanstom000608@gmail.com>
2025-05-20 00:42:54 +0900

    feat: card skeleton

---
+++ front_end/src/components/common/CardSkeleton/CardSkeleton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="w-full max-w-72 min-w-56 overflow-hidden rounded-2xl border border-white/20 bg-white/5"
+  >
+    <!-- 이미지 영역 -->
+    <div class="h-48 w-full animate-pulse bg-slate-500/40"></div>
+
+    <!-- 콘텐츠 영역 -->
+    <div class="space-y-3 p-3">
+      <div class="h-6 w-3/4 animate-pulse rounded-md bg-slate-500/50"></div>
+      <div class="h-4 w-full animate-pulse rounded-md bg-slate-500/40"></div>
+      <div class="flex justify-between">
+        <div class="h-5 w-20 animate-pulse rounded-full bg-slate-500/50"></div>
+        <div class="h-5 w-24 animate-pulse rounded-md bg-slate-500/40"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
