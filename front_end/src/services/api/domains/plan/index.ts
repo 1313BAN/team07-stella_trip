e94636f3b4088aa749374678ca200524f9f896ce
SeungYeon Han <hanstom000608@gmail.com>
2025-05-19 12:24:59 +0900

    feat: endpoints, api 호출 함수

---
+++ front_end/src/services/api/domains/plan/index.ts
@@ -0,0 +1,28 @@
+import { PLAN } from '../../endpoint';
+import api from '../../api';
+import type { ApiResponse, PagenationResponse } from '../../types';
+import type { Plan, PlansParams, Tag } from './types';
+
+/**
+ * query parameter를 바탕으로 여행 계획 리스트 조회
+ * @returns 여행 계획 리스트
+ */
+export const getPlans = async (params?: PlansParams): Promise<PagenationResponse<Plan>> => {
+  const response = await api.get<ApiResponse<PagenationResponse<Plan>>>(PLAN.PLANS, {
+    params,
+  });
+
+  return response.data.body;
+};
+
+/**
+ * 인기 많은 여행 계획 태그 리스트 조회
+ * @returns 여행 계획 태그 리스트
+ */
+export const getPopularTags = async (): Promise<Tag[]> => {
+  const response = await api.get<ApiResponse<Tag[]>>(PLAN.TAGS);
+
+  return response.data.body;
+};
+
+export type * from './types';
