ef09e8caae3b0ea4cb47d1612e5c541fdf56f479
SeungYeon Han <hanstom000608@gmail.com>
2025-05-20 00:43:06 +0900

    feat: tag card skeleton

---
+++ front_end/src/components/common/TagCardSkeleton/TagCardSkeleton.stories.ts
@@ -0,0 +1,30 @@
+// TagCardSkeleton.stories.ts
+import type { Meta, StoryObj } from '@storybook/vue3';
+import TagCardSkeleton from './TagCardSkeleton.vue';
+
+const meta: Meta<typeof TagCardSkeleton> = {
+  title: 'Components/common/TagCardSkeleton',
+  component: TagCardSkeleton,
+  tags: ['autodocs'],
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const Multiple: Story = {
+  render: () => ({
+    components: { TagCardSkeleton },
+    template: `
+      <div class="flex flex-wrap gap-4">
+        <TagCardSkeleton v-for="i in 3" :key="i" />
+      </div>
+    `,
+  }),
+};
