14f267b780e5b8fc62d2d44338d612e1995d1ba1
SeungYeon Han <hanstom000608@gmail.com>
2025-05-21 10:28:10 +0900

    feat: map error

--- front_end/src/components/map/MapError.vue
+++ front_end/src/components/map/MapError.vue
@@ -1,25 +1,14 @@
 <template>
-  <div class="h-full w-full p-4">
-    <ErrorContent
-      :error="error"
-      :resetError="resetError"
-      :showRetry="true"
-      :title="errorTitle || title + ' 오류'"
-      :description="errorDescription || '지도를 불러오는 중 문제가 발생했습니다'"
-    />
-  </div>
+  <ErrorContent
+    :error="error"
+    :resetError="resetError"
+    :showRetry="true"
+    :description="description || '지도를 불러오는 중 문제가 발생했습니다'"
+  />
 </template>
 
 <script setup lang="ts">
-import ErrorContent from '@/components/common/ErrorContent/ErrorContent.vue';
+import ErrorContent, { type ErrorProps } from '@/components/common/ErrorContent/ErrorContent.vue';
 
-interface MapErrorProps {
-  error: Error | null;
-  resetError: () => void;
-  title: string;
-  errorTitle?: string;
-  errorDescription?: string;
-}
-
-defineProps<MapErrorProps>();
+defineProps<ErrorProps>();
 </script>
