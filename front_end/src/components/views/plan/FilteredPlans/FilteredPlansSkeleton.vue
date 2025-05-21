97220fc3ea7e42ef489d9cb7d190705a5774642a
SeungYeon Han <hanstom000608@gmail.com>
2025-05-21 21:16:38 +0900

    feat: Filtered Plan

---
+++ front_end/src/components/views/plan/FilteredPlans/FilteredPlansSkeleton.vue
@@ -0,0 +1,10 @@
+<template>
+  <GridCardListContainer>
+    <CardSkeleton v-for="i in 10" :key="i" />
+  </GridCardListContainer>
+</template>
+
+<script setup lang="ts">
+import GridCardListContainer from '@/components/common/GridCardListContainer/GridCardListContainer.vue';
+import CardSkeleton from '@/components/skeleton/CardSkeleton/CardSkeleton.vue';
+</script>
