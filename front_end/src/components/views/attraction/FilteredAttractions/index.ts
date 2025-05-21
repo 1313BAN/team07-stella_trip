c61a662a454fa641e5c969275117e28448a2093a
SeungYeon Han <hanstom000608@gmail.com>
2025-05-21 21:06:48 +0900

    fix: attraction 오류들 수정

--- front_end/src/components/views/attraction/FilteredAttractions/index.ts
+++ front_end/src/components/views/attraction/FilteredAttractions/index.ts
@@ -1,2 +1,3 @@
 export { default as FilteredAttractions } from './FilteredAttractions.vue';
 export { default as FilteredAttractionsSkeleton } from './FilteredAttractionsSkeleton.vue';
+export { default as FilteredAttractionsError } from './FilteredAttractionsError.vue';
