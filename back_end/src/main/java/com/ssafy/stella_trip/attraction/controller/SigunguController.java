668fa1e38369a6304f7b4bb1c0ac5b0d6201e710
luna156 <v010309@naver.com>
2025-05-21 14:32:39 +0900

    docs: swagger 문서 추가

--- back_end/src/main/java/com/ssafy/stella_trip/attraction/controller/SigunguController.java
+++ back_end/src/main/java/com/ssafy/stella_trip/attraction/controller/SigunguController.java
@@ -4,6 +4,9 @@ package com.ssafy.stella_trip.attraction.controller;
 import com.ssafy.stella_trip.attraction.dto.response.SigunguResponseDTO;
 import com.ssafy.stella_trip.attraction.service.SigunguService;
 import com.ssafy.stella_trip.common.response.CommonResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -20,6 +23,13 @@ public class SigunguController {
     private final SigunguService sigunguService;
 
     @GetMapping("/sigungu")
+    @Operation(
+            summary = "시군구 정보 조회",
+            description = "시군구 정보 조회 api입니다."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "정상적으로 조회 완료"),
+    })
     public CommonResponse<SigunguResponseDTO> getSigungu() {
         return new CommonResponse<>(sigunguService.getSigungu(), HttpStatus.OK);
     }
