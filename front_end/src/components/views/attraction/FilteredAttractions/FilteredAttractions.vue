a189006e7c028462b5384178ba9bed7e1cadaac2
SeungYeon Han <hanstom000608@gmail.com>
2025-05-21 14:58:04 +0900

    feat: 필터링된 여행지 리스트 없는 경우 처리

--- front_end/src/components/views/attraction/FilteredAttractions/FilteredAttractions.vue
+++ front_end/src/components/views/attraction/FilteredAttractions/FilteredAttractions.vue
@@ -37,15 +37,22 @@ watch(
 </script>
 
 <template>
-  <GridCardListContainer :showTitle="false">
-    <AttractionCard
-      v-for="attraction in attractions"
-      :key="attraction.attractionId"
-      :attraction="attraction"
-      :showRating="true"
-      @cardClick="emit('cardClick', attraction)"
-      @likeClick="emit('likeClick', attraction)"
-      @tagClick="emit('tagClick', attraction.contentType)"
-    />
-  </GridCardListContainer>
+  <template v-if="attractions.length === 0">
+    <div class="h-full w-full pt-16 text-center">
+      <p class="text-lg text-gray-200">검색 결과가 없습니다.</p>
+    </div>
+  </template>
+  <template v-else>
+    <GridCardListContainer :showTitle="false">
+      <AttractionCard
+        v-for="attraction in attractions"
+        :key="attraction.attractionId"
+        :attraction="attraction"
+        :showRating="true"
+        @cardClick="emit('cardClick', attraction)"
+        @likeClick="emit('likeClick', attraction)"
+        @tagClick="emit('tagClick', attraction.contentType)"
+      />
+    </GridCardListContainer>
+  </template>
 </template>
